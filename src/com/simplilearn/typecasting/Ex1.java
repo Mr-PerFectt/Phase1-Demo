0a97210744f5e70a8b811ec7534c06b227885e67
ubuntu <ubuntu@ip-172-31-65-195.ec2.internal>
2020-07-26 07:39:40 +0000

    exercise done by Suvam Mohapatra

--- src/com/simplilearn/typecasting/Ex1.java
+++ src/com/simplilearn/typecasting/Ex1.java
@@ -1,11 +1,27 @@
 package com.simplilearn.typecasting;
 
-public class Ex1 {
+import java.util.Scanner;
+public class Ex1{
+private static Scanner myInput;
 
-	public static void main(String[] args) {
-		// WAP to take user input as int value and convert 
-		// into float, double and byte.
+public static void main( String args[] ) {
+myInput = new Scanner( System.in );
+int a;
 
-	}
+System.out.print( "Enter integer value: " );
+a = myInput.nextInt();
 
+
+float floata = a;
+double doublea = floata;
+byte bytea = (byte) doublea;
+
+
+
+System.out.println("Float value :"+floata);
+System.out.println("Double value :"+doublea);
+System.out.println("Byte value :"+bytea);
+
+
+}
 }
--- src/com/simplilearn/typecasting/Ex2.java
+++ src/com/simplilearn/typecasting/Ex2.java
@@ -1,11 +1,33 @@
 package com.simplilearn.typecasting;
+import java.util.Scanner ;
 
 public class Ex2 {
 
-	public static void main(String[] args) {
-		// WAP for taking user Input as string
-		// Integer Byte Float and Double
+private static Scanner input;
 
-	}
+public static void main(String[] args) {
+// WAP for taking user Input as string
+// Integer Byte Float and Double
+
+input = new Scanner(System.in);
+System.out.print("User Input as String :");
+
+String userNumber = input.nextLine();
+
+int userIntValue = Integer.parseInt(userNumber);
+byte userByteValue = Byte.parseByte(userNumber);
+
+float userFloatValue = Float.parseFloat(userNumber);
+
+double userDoubleValue = Double.parseDouble(userNumber);
+
+System.out.println("User Input as Interger : "+userIntValue);
+System.out.println("User Input as Byte : "+userByteValue);
+System.out.println("User Input as Float : "+userFloatValue);
+System.out.println("User Input as Double : "+userDoubleValue);
+
+
+
+}
 
 }
